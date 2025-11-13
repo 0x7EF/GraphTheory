11d5d6040ccde532f3942640060ea41e820b63ec
0x7EF <ma_benbouzid@esi.dz>
2025-11-13 23:57:41 +0100

    Added the basic structure of the Graph

--- graphlib.hpp
+++ graphlib.hpp
@@ -11,7 +11,7 @@ class GraphNode {
 private:
     std::string Label;
     std::unordered_map<GraphNode*, double> Neighbors;
-    int Degree;
+    size_t Degree;
 
 public:
     GraphNode(std::string label="") {
@@ -57,9 +57,59 @@ public:
 
 class Graph {
 private:
-    int V;
-    int E;
+    size_t V;
+    size_t E;
+    std::vector<GraphNode*> Vertices;
 
+public:
+    Graph() {
+        this->V = 0;
+        this->E = 0;
+        this->Vertices = {};
+    }
+
+    Graph(std::vector<GraphNode*> nodes) {
+        this->V = nodes.size();
+        for (size_t i = 0; i < nodes.size(); ++i) {
+            this->Vertices.push_back(nodes[i]);
+            this->E += nodes[i]->getDegree();
+        }
+    }
+
+    size_t getV() {
+        return this->V;
+    }
+
+    size_t getE() {
+        return this->E;
+    }
+
+    std::vector<GraphNode*> getVertices() {
+        return this->Vertices;
+    }
+
+    void addVertex(GraphNode* vertex) {
+        this->V += 1;
+        this->E += vertex->getDegree();
+        this->Vertices.push_back(vertex);
+    }
+    
+    void removeVertex(GraphNode* vertex) {
+        auto it = std::find(this->Vertices.begin(), this->Vertices.end(), vertex);
+        if (it == this->Vertices.end()) {
+            return;
+        }
+
+        this->Vertices.erase(it);
+        this->V -= 1;
+        this->E -= vertex->getDegree();
+        for (size_t i = 0; i < this->Vertices.size(); ++i) {
+            if (this->Vertices[i]->isNeighbor(vertex)) {
+                this->Vertices[i]->removeNeighbor(vertex);
+                this->E -= 1;
+            }
+        }
+    }
 };
 
 
